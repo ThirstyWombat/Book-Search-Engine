1dc919c85aa4f61a014b8e95098d0ab6c20cc9d2
Vincent Baraga <vincentb.baraga@gmail.com>
2024-01-31 18:14:00 -0500

    added apollo server and resolvers for user id

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
