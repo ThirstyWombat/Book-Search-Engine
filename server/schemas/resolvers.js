cbb46920fc8db23996d0e304c6c22af25d5df079
Vincent Baraga <vincentb.baraga@gmail.com>
2024-02-02 15:02:52 -0500

    added login, saveBook, and removeBook mutation to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const { findOne } = require("../models/User");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
@@ -14,6 +15,54 @@ const resolvers = {
       throw AuthenticationError;
     },
   },
-};
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      console.log(user);
+      const token = signToken(user);
 
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { args }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+// resolvers.Mutation.addUser({name:"vincent"})
 module.exports = resolvers;
