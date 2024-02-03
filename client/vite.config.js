b9146452d67d1be86b4b5e5338879ce0e54126e2
Vincent Baraga <vincentb.baraga@gmail.com>
2024-02-03 18:20:43 -0500

    vite config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/graphql": {
-        target: "http://localhost:3001",
+        target: "https://book-search-engine-server.onrender.com",
         secure: false,
         changeOrigin: true,
       },
