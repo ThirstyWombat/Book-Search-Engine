ff5ddf5b6c6bb2f5196307355fd728b259d7c1a1
Vincent Baraga <vincentb.baraga@gmail.com>
2024-02-03 17:24:27 -0500

    changed mongodb connection url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://me:AtLeast1Letter@googlebooks.pdkvlmi.mongodb.net/googlebooks"
+);
 
 module.exports = mongoose.connection;
